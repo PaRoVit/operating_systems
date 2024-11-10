200e1937383c436b84643ddb0cf81e195119ce0c
PaRoVit <144375362+PaRoVit@users.noreply.github.com>
2024-11-10 19:16:48 +0300

    Update child1.cpp

--- lab3/src/child1.cpp
+++ lab3/src/child1.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
         std::cerr << "Wrong argc in child1\n";
         exit(-6);
     }
-    const char* semaphoreName = argv[0];
-    const char* mmapFilename = argv[1];
+    const char* semaphoreName = argv[1];
+    const char* mmapFilename = argv[2];
     ProcessChild(semaphoreName, mmapFilename);
 }
