54b3bd501823182359c8be87cb0e252f3b031660
parovit <pavel.rozhkov2003@yandex.ru>
2024-11-17 19:28:09 +0300

    delete comment code

--- lab3/src/child2.cpp
+++ lab3/src/child2.cpp
@@ -1,7 +1,6 @@
 #include <utils.hpp>
 
 int main(int argc, char* argv[]) {
-    // std::cerr << argv[0]<< ' '<< argv[1] << "\n";
     if (argc < 2) {
         std::cerr << "Wrong argc in child1\n";
         exit(-6);
